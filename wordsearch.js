d9ca6d1418877373878aa5c43632c48a6a1e34ac
Bernard Yang <ybernard@gmail.com>
2021-06-03 01:29:59 +0000

    pair programming with Lana

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+// pair programming with Lana
+const wordSearch = (letters, word) => {
+    if (letters.length !== 0) {
+        let verticalArray = [];
+        for (let i = 0; i < letters[0].length; i++) {
+            verticalArray.push([]);
+        }
+        for (i = 0; i < letters.length; i++) {
+            for (j = 0; j < letters[i].length; j++) {
+                verticalArray[j].push(letters[i][j]);
+            }
+        }
+        const verticalJoin = verticalArray.map(ls => ls.join(''));
+        const horizontalJoin = letters.map(ls => ls.join(''));
+        for (let column of verticalJoin) {
+            for (let row of horizontalJoin) {
+                if (column.includes(word) && row.includes(word)) {
+                    return true;
+                }
+            }
+        }
     }
-}
+    return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
